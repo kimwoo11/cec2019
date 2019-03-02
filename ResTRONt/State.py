a60788a270a6aabaeba676ad8199f6ecbf97157f
Steve Kim <steve@v1040-wn-rt-a-113-112.campus-dynamic.uwaterloo.ca>
2019-03-02 13:50:30 -0500

    dlafksdj

--- ResTRONt/State.py
+++ ResTRONt/State.py
@@ -2,12 +2,13 @@ class State():
     ''' State '''
     def __init__(self):
         curr_map = []
-        cost_query = {}
+        costQuary = {}
         remaining_trash = {}
         time_spent = 0
         time_to_collect = 0
         bin_cap = {}
-        bin_count = {}
+        bin_count = {0: 0, 1:0, 2:0}
+        finished = False
 
     def init_map(self, data):
         data = data["payload"]["constants"]
@@ -18,6 +19,9 @@ class State():
             x = data['BIN_LOCATION'][b_type]['X']
             y = data['BIN_LOCATION'][b_type]['Y']
             self.curr_map[y][x] = {'B': i}
+            self.bin_cap[i, data['BIN_CAPACITY'][b_type]]
+            self.remaining_trash[i, data['BIN_CAPACITY']]
 
     def update_map(self, data):
-        
+        data = data["payload"]["constants"]
+
