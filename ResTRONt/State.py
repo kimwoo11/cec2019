bb12b882068d0be684a9fa51d6b56ed34d5d0640
Steve Kim <steve@v1040-wn-rt-a-113-112.campus-dynamic.uwaterloo.ca>
2019-03-02 13:00:45 -0500

    state

--- ResTRONt/State.py
+++ ResTRONt/State.py
@@ -1,6 +1,23 @@
 class State(object):
     def __init__(self):
         curr_map = []
-        costQuery = {}
+        cost_query = {}
+        remaining_trash = {}
+        time_spent = 0
+        time_to_collect = 0
+        bin_cap = {}
+        bin_count = {}
 
-    def init(self, rows, col):
+    def init_map(self, data):
+        data = data["payload"]["constants"]
+        y = data["ROOM_DIMENSIONS"]["Y_MAX"]
+        x = data["ROOM_DIMENSIONS"]["X_MAX"]
+        self.curr_map = [[{'U': 'U'} for i in range(x)] for j in range(y)]
+        for i, b_type in enumerate(['GARABAGE', 'RECYCLE', 'ORGANIC']):
+            x = data['BIN_LOCATION'][b_type]['X']
+            y = data['BIN_LOCATION'][b_type]['Y']
+            self.curr_map[y][x] = {'B': i}
+
+    def update_map(self, data):
+        
+
--- ResTRONt/main.py
+++ ResTRONt/main.py
@@ -4,5 +4,12 @@ from State import *
 from util import *
 
 if __name__ == "__main__":
-    print(Request.get('instance'))
-    print(Request.post('instance'))
+	data = Request.get('instance')
+	curr_state = State.init_map(data)
+	robot = Robot()	
+	finished = data['payload']['finished']
+	while not finished:
+		next_instr = eval_function(curr_state, robot)
+		# robot executes
+		# state is updated
+		
